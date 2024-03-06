6239b8a23c3a1e629a47155289abcb84e77218e9
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2024-03-06 22:14:39 +0530

    12

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const heading1=React.createElement("h1",{},"hello my world")
-const heading2=React.createElement("h2",{},"hello my india")
+import React from 'react'
+import ReactDOM from "react-dom/client";
+
+
+
+const heading1=React.createElement("h1",{},"welcome to new unboxing vedio")
+const heading2=React.createElement("h2",{},"welcome to the coding world")
 
 const container=React.createElement("div",{},[heading1,heading2]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
